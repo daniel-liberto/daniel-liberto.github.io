a3f2470aed47a6ddfc945b9d7c8f41010762d6d3
daniel-liberto <daniel.liberto@hotmail.com>
2022-11-09 15:52:33 -0300

    correcoes no mobile js

--- script/scrollAnima.js
+++ script/scrollAnima.js
@@ -1,20 +1,30 @@
 function initAnimacaoScroll() {
   const sections = document.querySelectorAll(".js-scroll > div");
+  const media = matchMedia("min-width: 760px").matches;
   if (sections.length) {
-    const windowFadeIn = window.innerHeight * 0.6;
-    const windowFadeOut = window.innerHeight * 0.03;
-    // console.log(windowMetade);
+    const FadeIn = window.innerHeight * 0.6;
+    const FadeOut = window.innerHeight * 0.03;
+
+    const FadeInMobile = window.innerHeight * 0.8;
+    const FadeOutMobile = window.innerHeight * 0.03;
 
     function animaScroll() {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
-        // console.log(sectionTop);
-
-        if (sectionTop - windowFadeIn < 0) {
-          section.classList.add("ativo");
-        }
-        if (sectionTop < -windowFadeOut) {
-          section.classList.remove("ativo");
+        if (media) {
+          if (sectionTop - FadeIn < 0) {
+            section.classList.add("ativo");
+          }
+          if (sectionTop < -FadeOut) {
+            section.classList.remove("ativo");
+          }
+        } else {
+          if (sectionTop - FadeInMobile < 0) {
+            section.classList.add("ativo");
+          }
+          if (sectionTop < -FadeOutMobile) {
+            section.classList.remove("ativo");
+          }
         }
       });
     }
